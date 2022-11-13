3cb4a8ce05bc408b5aeca9a697cc1265a03a65f5
na7hk3r <elmathi7@gmail.com>
2022-11-13 16:03:11 -0300

    Correcciones

--- scripts/main.js
+++ scripts/main.js
@@ -16,13 +16,12 @@ let btnModificar = document.getElementById("btnPut");
 let btnEliminar = document.getElementById("btnDelete");
 let btnGuardar = document.getElementById("btnSendChanges");
 
-// 
+//
 
-let urlUsers = "https://6362c2c937f2167d6f6c7564.mockapi.io/users"
+let urlUsers = "https://6362c2c937f2167d6f6c7564.mockapi.io/users";
 // para get, post
-let urlID = "https://6362c2c937f2167d6f6c7564.mockapi.io/users/:id"
+let urlID = "https://6362c2c937f2167d6f6c7564.mockapi.io/users/:id";
 // para  get1, put, delete
-
 
 let lista = [];
 let result = document.getElementById("results");
@@ -30,32 +29,29 @@ let listaHTML = "";
 
 function resultado(lista) {
   lista.forEach((lista) => {
-
     listaHTML += `
         <li>ID: ${lista.id}</li>
         <li>Nombre: ${lista.name}</li>
         <li>Apellido: ${lista.lastname}</li>
-        `
+        `;
     result.innerHTML = listaHTML;
-  })
-};
+  });
+}
 
 //búsqueda
 btnBuscar.addEventListener("click", () => {
-  let id = inputGetId.value
+  let id = inputGetId.value;
   if (id != "" || id > 0) {
-
     fetch("https://6362c2c937f2167d6f6c7564.mockapi.io/users/" + id)
       .then((response) => response.json())
       .then((data) => {
-        lista = data
+        lista = data;
         result.innerHTML = `
        <li>ID: ${lista.id}</li>
        <li>Nombre: ${lista.name}</li>
        <li>Apellido: ${lista.lastname}</li>
-       `
-      })
-
+       `;
+      });
   } else {
     fetch(urlUsers)
       .then((response) => response.json())
@@ -63,47 +59,37 @@ btnBuscar.addEventListener("click", () => {
         lista = data;
         console.log(data);
         resultado(lista);
-
       });
   }
 
-  if( id !== lista.id && id !== "") {
-    swal ( "Vaya" ,  "Parece que el ID solicitado no existe" ,  "error" )
-    }
-    
+  if (id !== lista.id && id !== "") {
+    swal("Vaya", "Parece que el ID solicitado no existe", "error");
+  }
 });
 
 //agregar
 btnAgregar.addEventListener("click", () => {
-
   let nombre = inputPostNombre.value;
   let apellido = inputPostApellido.value;
   if (nombre != "" || apellido != "")
-
     fetch("https://6362c2c937f2167d6f6c7564.mockapi.io/users/", {
-
-      headers: { 'Content-Type': 'application/json' },
-      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
       body: JSON.stringify({
         name: nombre,
         lastname: apellido,
-      })
+      }),
     })
-
       .then((response) => response.json())
       .then((data) => {
-
-        lista = data
+        lista = data;
         resultado(lista);
+      });
 
-
-      })
-
-      if( id !== lista.id && id !== "") {
-        swal ( "Vaya" ,  "Parece que el ID solicitado no existe" ,  "error" )
-        }
-
-})
+  if (id !== lista.id && id !== "") {
+    swal("Vaya", "Parece que el ID solicitado no existe", "error");
+  }
+});
 
 // para modificar un usuario
 btnModificar.addEventListener("click", () => {
@@ -143,58 +129,49 @@ btnGuardar.addEventListener("click", () => {
 
 //eliminar usuario
 btnEliminar.addEventListener("click", () => {
-  let id = inputDelete.value
+  let id = inputDelete.value;
   if (id != "" || id > 0) {
-
     fetch("https://6362c2c937f2167d6f6c7564.mockapi.io/users/" + id, {
       method: "DELETE",
     })
-      .then(res => res.json())
-      .then(data => {
-
-        lista = data
+      .then((res) => res.json())
+      .then((data) => {
+        lista = data;
         resultado(lista);
+        location.reload();
       });
   }
 
-  if( id !== lista.id && id !== "") {
-    swal ( "Vaya" ,  "Parece que el ID solicitado no existe" ,  "error" )
-    }
-
+  if (id !== lista.id && id !== "") {
+    swal("Vaya", "Parece que el ID solicitado no existe", "error");
+  }
 });
-
-
 
 //Botones disabled
 
-btnAgregar.disabled = true
-btnModificar.disabled = true
-btnEliminar.disabled = true
+btnAgregar.disabled = true;
+btnModificar.disabled = true;
+btnEliminar.disabled = true;
 inputPostNombre.addEventListener("change", () => {
-
   if (inputPostNombre.value === "" && inputPostApellido.value === "") {
-    btnAgregar.disabled = true
+    btnAgregar.disabled = true;
   } else {
-    btnAgregar.disabled = false
+    btnAgregar.disabled = false;
   }
-
-})
+});
 
 inputPutId.addEventListener("change", () => {
   if (inputPutId.value === "") {
-    btnModificar.disabled = true
+    btnModificar.disabled = true;
   } else {
-    btnModificar.disabled = false
+    btnModificar.disabled = false;
   }
-
-})
+});
 
 inputDelete.addEventListener("change", () => {
   if (inputDelete.value === "") {
-    btnEliminar.disabled = true
+    btnEliminar.disabled = true;
   } else {
-    btnEliminar.disabled = false
-
+    btnEliminar.disabled = false;
   }
-
 });
